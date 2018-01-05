00afa4ae85405b537d241506dd14852d0bf0c14a
GatorSutton <garrettsuttonee@gmail.com>
2018-01-05 01:19:00 -0500

    dragon and animations start

---
+++ DragonBossFight/Assets/Scripts/ArduinoCommunicator.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * SerialCommUnity (Serial Communication for Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+/**
+ * Sample for reading using polling by yourself, and writing too.
+ */
+public class ArduinoCommunicator : MonoBehaviour
+{
+    public SerialController serialController;
+    string messageIN;
+    string messageOUT;
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+    }
+
+    // Executed each frame
+    void Update()
+    {
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        string message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(messageIN, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(messageIN, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+            Debug.Log("Message arrived: " + messageIN);
+        messageIN = message;
+
+        //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+        serialController.SendSerialMessage(messageOUT);
+
+    }
+
+    public void setMessageOUT(string data)
+    {
+        messageOUT = data;
+    }
+
+    public bool[] getMessageIN()
+    {
+        return messageIN.Select(x => x == '1').ToArray();
+    }
+}
---
+++ DragonBossFight/Assets/Scripts/dragonExitController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonExitController : MonoBehaviour {
+
+    private SplineWalker SW;
+    public bool actionComplete = false;
+    public Material material;
+
+    // Use this for initialization
+    void Awake () {
+        SW = GetComponent<SplineWalker>();
+        actionComplete = false;
+    }
+
+    private void OnEnable()
+    {
+         StartCoroutine(beginExit());
+    }
+
+    private void OnDisable()
+    {
+        actionComplete = false;
+    }
+
+    private IEnumerator beginExit()
+    {
+        material.color = Color.red;
+        SW.assignPath("path");
+        SW.setPosition(SplineWalker.Position.Front);
+
+        while (!SW.isPositionReached())
+        {
+            yield return null;
+        }
+        SW.assignPath("entrance");
+        SW.setPosition(SplineWalker.Position.Left);
+        SW.setProgress(1f);
+        while (!SW.isPositionReached())
+        {
+            yield return null;
+        }
+        actionComplete = true;
+    }
+
+}
