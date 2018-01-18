21446bb49fa3f7cf6a1e356270735a9353cd7704
GatorSutton <garrettsuttonee@gmail.com>
2018-01-18 17:46:49 -0500

    file organization

---
+++ DragonBossFight/Assets/Scripts/FireballAnimationEnder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAnimationEnder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       foreach(Transform child in transform)
+        {
+            Destroy(child.gameObject, .5f);
+        }
+    }
+}
