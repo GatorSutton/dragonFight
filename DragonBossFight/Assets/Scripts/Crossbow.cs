12af390d1f63bf02c1968e7a5119ff3aa0ca3941
GatorSutton <garrettsuttonee@gmail.com>
2018-02-28 05:59:41 -0500

    post processing and redoing torches

---
+++ DragonBossFight/Assets/Scripts/Crossbow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WiimoteApi;
+
+public class Crossbow : MonoBehaviour {
+
+    public float coolDownTime;
+
+    private Wiimote wiimote;
+    private float cdRemaining;
+    private float[] pointer;
+    public float offsetRotation = 0;
+
+    // Use this for initialization
+    void Start () {
+        cdRemaining = coolDownTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!WiimoteManager.HasWiimote()) { return; }
+
+
+        wiimote = WiimoteManager.Wiimotes[0];
+        pointer = wiimote.Ir.GetPointingPosition();
+
+
+        if (wiimote.Button.b && cdRemaining < 0 && pointer[0] > -.5)
+        {
+            cdRemaining = coolDownTime;
+            Debug.DrawRay(transform.position, calculateArrowVector()*100, Color.green);
+
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, calculateArrowVector());
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                if (hit.collider.tag == "target")
+                {
+                    //hit.collider.transform.root.GetComponent<dragonHealth>().takeDamage();
+                    hit.collider.gameObject.GetComponent<targetController>().takeHit();
+                }
+            }
+        }
+
+        cdRemaining -= Time.deltaTime;
+
+
+
+    }
+
+    Vector3 calculateArrowVector()
+    {
+        Vector3 vector = new Vector3((pointer[0] - .5f)*2f*.736f, (pointer[1] - .5f)*2f*.414f, 1f) - this.transform.position;    //scaled with the camera size change with variables to have 
+        vector = Quaternion.Euler(0, offsetRotation, 0) * vector;                                                                //it work with different aspect ratios. Need aspect ratio to
+        return vector;                                                                                                          //determine horizontal field of view angle. Then use that with 1
+                                                                                                                                //as adjacent triangle length to determine opposite for both vert
+    }                                                                                                                           // and horizontal to get the scalars.
+
+
+}
